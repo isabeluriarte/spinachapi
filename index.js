63d2951c8c4411eda87f31260cc863009fb74c68
Isabel Uriarte <uriarte.isabel@gmail.com>
2020-01-08 10:48:30 -0800

    added necessary files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my API");
+});
+
+app.get("/spinach", (req, res) => {
+    res.json({
+        name: "spinach",
+        bestFood: true
+    });
+});
+
+app.listen(PORT, function(){
+    console.log("listening on port" + PORT);
+});
